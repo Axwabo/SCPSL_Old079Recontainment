b2ca5ffaf9780045c1f3c5b6e61999472cc63cee
BuildTools <unconfigured@null.spigotmc.org>
2022-01-15 09:35:41 +0100

    First commit

---
+++ Old079Recontainment/Plugin079.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using HarmonyLib;
+using MapEvents = Exiled.Events.Handlers.Map;
+
+namespace Old079Recontainment {
+    public class Plugin079 : Plugin<Config079> {
+        public static Plugin079 Singleton { get; private set; }
+        private Harmony _harmony;
+
+        public Plugin079() {
+            Singleton = this;
+        }
+
+        public override void OnEnabled() {
+            base.OnEnabled();
+            _harmony = new Harmony("mc.axwabo.old079");
+            try {
+                _harmony.PatchAll();
+            } catch (Exception e) {
+                Log.Error("Patching failed!\n" + e);
+                return;
+            }
+
+            MapEvents.Generated += EventHandlers.MapGenerated;
+        }
+
+        public override void OnDisabled() {
+            base.OnDisabled();
+            _harmony.UnpatchAll();
+        }
+
+        public override void OnReloaded() {
+            base.OnReloaded();
+        }
+
+        public override string Name { get; } = "Old079Recontainment";
+        public override string Author { get; } = "Axwabo";
+        public override PluginPriority Priority { get; } = PluginPriority.Highest;
+        public override Version Version { get; } = new Version(1, 0, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(4, 2);
+
+        public static string CassieGlitchJam(string message, float glitchChance, float jamChance) {
+            var strArray = message.Split(' ');
+            var newWords = new List<string>();
+            newWords.EnsureCapacity(strArray.Length);
+            for (var index = 0; index < strArray.Length; ++index) {
+                newWords.Add(strArray[index]);
+                if (index >= strArray.Length - 1) continue;
+                if (UnityEngine.Random.value < (double) glitchChance)
+                    newWords.Add(".G" + UnityEngine.Random.Range(1, 7));
+                if (UnityEngine.Random.value < (double) jamChance)
+                    newWords.Add("JAM_" + UnityEngine.Random.Range(0, 70).ToString("000") + "_" +
+                                 UnityEngine.Random.Range(2, 6));
+            }
+
+            return newWords.Aggregate("", (current, newWord) => current + newWord + " ");
+        }
+    }
+}
---
+++ Old079Recontainment/Reflection.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+namespace Axwabo.Util {
+    public static class Reflection {
+        public static T Get<T>(this object obj, string field) {
+            return (T) Get(obj, field);
+        }
+
+        public static object Get(this object obj, string field) {
+            return obj.GetType().Get(obj, field);
+        }
+
+        public static void Set(this object obj, string field, object val) {
+            obj.GetType().Set(obj, field, val);
+        }
+
+        public static T Call<T>(this object obj, string methodName, params object[] args) {
+            return obj.GetType().Call<T>(obj, methodName, args);
+        }
+
+        public static void Call(this object obj, string methodName, params object[] args) {
+            Call<object>(obj, methodName, args);
+        }
+
+        public static T StaticGet<T>(this Type type, string name) {
+            return (T) StaticGet(type, name);
+        }
+
+        public static object StaticGet(this Type type, string name) {
+            var field = type.GetField(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            return field != null ? field.GetValue(null) : default;
+        }
+
+        public static void StaticSet(this Type type, string field, object val) {
+            var fieldInfo = type.GetField(field, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            if (fieldInfo != null)
+                fieldInfo.SetValue(null, val);
+        }
+
+        public static T StaticCall<T>(this Type type, string methodName, params object[] args) {
+            var method = type.GetMethod(methodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+            return method != null ? (T) method.Invoke(null, args) : default;
+        }
+
+        public static void StaticCall(this Type type, string methodName, params object[] args) {
+            StaticCall<object>(type, methodName, args);
+        }
+
+
+        public static T Get<T>(this Type type, object obj, string field) {
+            return (T) Get(type, obj, field);
+        }
+
+        public static object Get(this Type type, object obj, string field) {
+            var fieldInfo = type.GetField(field,
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance);
+            return fieldInfo?.GetValue(obj);
+        }
+
+        public static void Set(this Type type, object obj, string field, object val) {
+            var fieldInfo = type.GetField(field,
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance);
+            if (fieldInfo != null)
+                fieldInfo.SetValue(obj, val);
+        }
+
+        public static T Call<T>(this Type type, object obj, string methodName, params object[] args) {
+            var method = type.GetMethod(methodName,
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.CreateInstance);
+            return method != null ? (T) method.Invoke(obj, args) : default;
+        }
+
+        public static void Call(this Type type, object obj, string methodName, params object[] args) {
+            Call<object>(type, obj, methodName, args);
+        }
+
+        public static IEnumerable<T> MapUnknownTypeArray<T>(this object obj, string fieldName, string innerField,
+            Func<object, T> mapper) {
+            return obj.MapUnknownTypeArray(fieldName, o => mapper(o.Get(innerField)));
+        }
+
+        public static IEnumerable<T> MapUnknownTypeArray<T>(this object obj, string fieldName, Func<object, T> mapper) {
+            return (from object o in obj.Get<IEnumerable>(fieldName) select mapper(o)).ToList();
+        }
+
+        public static IEnumerable<T> Enums<T>(this Type type) {
+            return Enum.GetValues(type).ToArray<T>();
+        }
+
+        public static T Construct<T>(this Type type, params object[] args) {
+            var constructor = type.GetConstructors().FirstOrDefault(e => {
+                var list = e.GetParameters().AsEnumerable().ToList();
+                if (args.Length != list.Count)
+                    return false;
+                for (var i = 0; i < list.Count; i++) {
+                    var o = args[i];
+                    if (o != null && !list[i].ParameterType.IsInstanceOfType(o))
+                        return false;
+                }
+
+                return true;
+            });
+            return constructor == null ? default : (T) constructor.Invoke(args);
+        }
+    }
+}
