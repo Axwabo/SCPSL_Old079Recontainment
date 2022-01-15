79b67853f2e367c58a8683a8885a1544de847eb0
BuildTools <unconfigured@null.spigotmc.org>
2022-01-15 10:42:58 +0100

    Add automatic recontainment feature

---
+++ Old079Recontainment/AnnouncementPatch.cs
@@ -0,0 +1,10 @@
+﻿using HarmonyLib;
+
+namespace Old079Recontainment {
+    [HarmonyPatch(typeof(Recontainer079),"UpdateStatus")]
+    internal static class AnnouncementPatch {
+        private static void Postfix(int engagedGenerators) {
+            
+        }
+    }
+}
