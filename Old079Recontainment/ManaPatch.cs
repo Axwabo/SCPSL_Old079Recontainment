cd64beedc2173279e5228779fba19feaae7cec3b
BuildTools <unconfigured@null.spigotmc.org>
2022-01-16 11:24:02 +0100

    Add old AP generation before overcharge

---
+++ Old079Recontainment/ManaPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Old079Recontainment {
+    [HarmonyPatch(typeof(Scp079PlayerScript), "ServerUpdateMana")]
+    internal static class ManaPatch {
+        private static void Postfix(Scp079PlayerScript __instance) {
+            if (!Plugin079.DelayStopwatch.IsRunning)
+                return;
+            __instance.Network_curMana =
+                Mathf.Clamp(__instance.Network_curMana + Plugin079.Cfg.PowerPerSecond * Time.deltaTime, 0,
+                    __instance.levels[__instance.Lvl].maxMana);
+        }
+    }
+}
